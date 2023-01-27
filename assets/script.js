ccf9e0b856073aefd243bf639f3b03bf0b46050f
m1taylor <m1taylor@ryerson.ca>
2023-01-27 12:19:03 -0500

    set up values for time blocks to correspond with time

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,20 @@
-let currentDay = `${dayjs().format("dddd, MMM D YYYY")}`;
+//setting day/time display at top of schedule
+$(function () {
+    let timeNow = dayjs().format('dddd, MMM D YYYY HH:m a');
+    $("#currentDay").text(timeNow);
 
-let currentHour =  `${dayjs().format("H")}`;
+//Setting up values for time blocks in local stores
+$('#8 .description').val(localStorage.getItem('8'));
+$('#9 .description').val(localStorage.getItem('9'));
+$('#10 .description').val(localStorage.getItem('10'));
+$('#11 .description').val(localStorage.getItem('11'));
+$('#12 .description').val(localStorage.getItem('12'));
+$('#13 .description').val(localStorage.getItem('13'));
+$('#14 .description').val(localStorage.getItem('14'));
+$('#15 .description').val(localStorage.getItem('15'));
+$('#16 .description').val(localStorage.getItem('16'));
+$('#17 .description').val(localStorage.getItem('17'));
 
-var timeBlock = document.querySelector('.time-block');
-
-//date at top
-document.querySelector(".currentDay").innerHTML = currentDay;
+  });
 
 
-function bgColor() {
-  if (timeBlock.id == currentHour) {
-  timeBlock.classList.add(".present");
-  timeBlock.style.backgroundColor = "lightblue";
-  } else if (timeBlock.id > currentHour){
-    timeBlock.classList.add(".past");
-    timeBlock.style.backgroundColor = "lightblue";
-  } else {
-    timeBlock.classList.add(".future");
-  }
-}
-
-bgColor();
